937b813f9b34b11ea0a6b287a256ad341ac94216
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-29 18:13:37 +0300

    javascript basic

---
+++ 1Data_Types_And_Variables.js
@@ -0,0 +1,28 @@
+// Data Type And Variables
+// Declaring a variable using var
+var age = 30;
+
+document.write(age);
+
+// Declaring a variable using let
+letname = "John";
+
+// Declaring a variable using const
+const pi = 3.14;
+
+// Declaring and initializing an array
+let numbers = [1, 2, 3, 4, 5];
+
+// Declaring and initializing an object
+let person = {
+  name: "Jane",
+  age: 25,
+  city: "New York"
+};
+
+// Declaring and calling a function
+function sayHello(name) {
+  console.log("Hello, " + name + "!");
+}
+
+sayHello("Bob"); // Output: Hello, Bob!
---
+++ 2User_Input.js
@@ -0,0 +1,4 @@
+let name = prompt("What's your name?");
+console.log("Hello, " + name + "!");
+
+document.write(name);
