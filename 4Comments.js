ee3528528bbc8c53eb20920de624ba0eed083aab
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-29 18:34:17 +0300

    4Comments

---
+++ 4Comments.js
@@ -0,0 +1,11 @@
+// This is a single-line comment
+let x = 5; // This line sets the value of x to 5
+
+
+/*
+This is a multi-line comment.
+It can span multiple lines and is useful for providing longer explanations.
+*/
+
+let y = 10; /*
+This line sets the value of y to 10, but it's commented out for now. */
