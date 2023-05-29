936672d5109134783d266c00db1234a30f72edaf
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-29 18:43:20 +0300

    6If_And_If_else

---
+++ 6If_And_If_else.js
@@ -0,0 +1,18 @@
+
+let x = 10;
+
+if (x > 5) {
+  console.log("x is greater than 5");
+} else {
+  console.log("x is less than or equal to 5");
+}
+
+let y = 20;
+
+if (x > y) {
+  console.log("x is greater than y");
+} else if (x < y) {
+  console.log("x is less than y");
+} else {
+  console.log("x is equal to y");
+}
