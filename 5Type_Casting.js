0107e295fd4c7722fb5b516b54147e1a030495fd
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-29 18:38:57 +0300

    5Type_Casting

---
+++ 5Type_Casting.js
@@ -0,0 +1,20 @@
+let x1 = "10";
+let y1 = parseInt(x);
+console.log(y1); // Output: 10
+document.write(y1);
+
+let x2 = 10;
+let y2 = x.toString();
+console.log(y2); // Output: "10"
+document.write(y2);
+
+
+let x3 = true;
+let y3 = x.toString();
+console.log(y3); // Output: "true"
+document.write(y3);
+
+
+let x4 = "true";
+let y4 = Boolean(x4);
+console.log(y4); // Output: true
