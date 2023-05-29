071ced9998dcfd71fa5dc29ba058dcf7c0bc124f
Hatem <hatemahmedmohamed27@gmail.com>
2023-05-29 18:19:56 +0300

    3Operators

---
+++ 3Operators.js
@@ -0,0 +1,16 @@
+let x = 5;
+let y = 10;
+let z = x + y; // z = 15
+document.write(z);
+
+let br = document.createElement("br");
+document.body.appendChild(br);
+
+
+letisGreater = x > y; // isGreater = false
+document.write(letisGreater);
+
+let message = isGreater ? "x is greater than y" : "y is greater than x"; // message = "y is greater than x"
+
+x++; // x = 6
+y--; // y = 9
